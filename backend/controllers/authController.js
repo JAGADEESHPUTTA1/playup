7f6ac195867d0cff0a7b53067c739f55aa112bf3
Jagadeesh Putta <jagadeesh.putta1@gmail.com>
2026-01-01 22:07:43 +0530

    auth

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -164,7 +164,7 @@ export const verifyOtp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -198,4 +198,3 @@ export const logout = (req, res) => {
     message: "Logged out successfully",
   });
 };
-
