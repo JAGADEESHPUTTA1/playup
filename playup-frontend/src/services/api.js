c0e8e9d8e1956ede80fe729d31e8fd54ca461cec
JAGADEESHPUTTA1 <jagadeesh.putta1@gmail.com>
2026-01-29 00:12:52 +0530

    Update api.js

--- playup-frontend/src/services/api.js
+++ playup-frontend/src/services/api.js
@@ -2,6 +2,17 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, // 🔐 THIS IS THE KEY
+  withCredentials: true,
 });
 
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // 🔴 Redirect on ANY API failure
+    if (error.response) {
+      window.location.href = "/";
+    }
+
+    return Promise.reject(error);
+  }
+);
